eb2994d71c339ae53bd8c21c42d0e7707213e3ff
Dave Buscaglia <david.buscaglia@vts-i.com>
2023-10-20 09:28:56 -0700

    Day 20 completed!

--- 2020/day20/tiles.py
+++ 2020/day20/tiles.py
@@ -3,21 +3,6 @@ import numpy as np
 import collections
 
 
-
-def hasedge(edge, edges):
-	for e in edges:
-		if edge == e: return True
-		if edge == e[::-1]: return True
-	return False
-
-def findTileWithSide(side, tiles):
-	tlist = []
-	if side == None: return tlist
-	for t in tiles:
-		if t.used: continue
-		if hasedge(side, t.side) : tlist.append(t.num)
-	return tlist
-
 def findedge(tiles, excTile, thisedge):
 	#print("findedge: ", thistile.num, " side: ", thisedge)
 	for t in tiles:
@@ -107,6 +92,12 @@ class Tile:
 		print("   ", self.side[2])
 		print(" ")
 
+	def block(self):
+		print("Tile: ", self.num, "  CW: ", self.cw, "  Flipped: ", self.flipped)
+		for r in self.rows[1:9]:
+			print(" ", tostring(r)[1:9])
+		print(" ")
+
 	def mark(self, tiles):
 		count = 0
 		excTile = self.num
@@ -121,7 +112,8 @@ class Tile:
 
 
 	def rotate(self, cw):
-		self.cw = cw
+		self.cw += cw
+		self.cw %= 4
 		if (cw == 0):
 			return
 		self.rows = np.rot90(self.rows, k=cw, axes=(1,0))
@@ -149,57 +141,22 @@ class Tile:
 			self.side[3] = self.side[3][::-1]
 			self.side[1] = self.side[1][::-1]
 
-	def orientToSideOld(self, side, sno):
-		f = False
-		found = False
-		for r in range(4):
-			if self.side[r] == side:
-				found = True
-				break
-			if self.side[r][::-1] == side:
-				found = True
-				f = True
-				break
-		if not found:
-			print("ERROR - requested side: ", side, " not in this tile: ", self.num)
-
-		# need to rotate sno - r
-		cw = (sno - r + 4 ) % 4
-		print("cw: ", cw, " r = ", r)
-		self.rotate(cw)
-		#self.display()
-		if f and sno % 2: 
-			print("Vertical flip")
-			self.flip(True)
-		elif f : 
-			print("Horizontal flip")
-			self.flip(True)
-		#self.display()
 
 	def orientToSide(self, side, sno):
 		f = False
 		found = False
-		for r in range(4):
-			if self.side[sno] == side:
-				found = True
-				break
-			if self.side[sno][::-1] == side:
-				found = True
-				f = True
-				break
-			self.rotate(1)
+		for flipped in [False, True]:
+			for r in range(4):
+				if self.side[sno] == side:
+					found = True
+					break
+				self.rotate(1)
+			if found : break
+			self.flip(True)
 		if not found:
 			print("ERROR - requested side: ", side, " not in this tile: ", self.num)
 
-		#self.display()
-		if f:
-			if sno % 2 == 1: 
-				print("Vertical flip")
-				self.flip(True)
-			else: 
-				print("Horizontal flip")
-				self.flip(True)
-		#self.display()
+
 	
 	def hasSide(self, s, dbg = False):
 		found = False
@@ -219,87 +176,7 @@ class Tile:
 				break
 		if (dbg) : print ("found", found, "rot", rot, "flip", flip, sep = " ")
 		return found, rot, flip
-#
-# find a tile with top, right, bottom and/or left matches
-# return the tile number most found
-#	
-def findPiece(tiles, top, right, bottom, left):
-	tlist = []
-	needed = 0
-	c = collections.Counter()
-	if not top == None:
-		t = findTileWithSide(top, tiles)
-		for i in t: c[i] += 1
-		needed += 1
-		
-	if not right == None:
-		t = findTileWithSide(right, tiles)
-		for i in t: c[i] += 1
-		needed += 1
-		
-	if not bottom == None:
-		t = findTileWithSide(bottom, tiles)
-		for i in t: c[i] += 1
-		needed += 1
-		
-	if not left == None:	
-		t = findTileWithSide(left, tiles)
-		for i in t: c[i] += 1
-		needed += 1
-	
-	#for i in tlist:
-		#print(i)
-		#c[i] += 1
-	tn, tc = c.most_common(1)[0]
-	#tc = c.most_common(1)[0][0]
-	#if tc == needed:
-	#	print("Found tile: ", tn)
-	#print("tn: ", tn, "tc: ", tc, " ", c)
-	return tn
 
-		
-tiles = []
-f = open("data.txt", "r")
-l = f.readline()
-while not l == "":
-	tiles.append(Tile(f, l))
-	l = f.readline()
-
-
-#exit(1)
-
-prod = 1
-corners = []
-edges = []
-others = []
-for t in tiles:
-	t.mark(tiles)
-	if (t.type == 2) : 
-		prod *= t.num ## Part 1 corner product
-		#print(t)
-		corners.append(t)
-		
-		#print(t.num, " ", t.xside)
-	elif (t.type == 3) :
-		edges.append(t)
-	else:
-		others.append(t)
-
-
-print("Part 1: Product is: ", prod)
-
-
-
-##
-## needed for Part 2
-numpixels = math.floor(math.sqrt(len(tiles)) + 0.5)
-print("Numpixels: ", numpixels)
-upperLeftCorner = ([False,True,True,False])
-upperRightCorner = ([False,False,True,True])
-lowerRightCorner = ([True,False,False,True])
-lowerLeftCorner = ([True,True,False,False])
-puzzle = np.zeros((numpixels, numpixels))
-c = 0  # first corner
 
 ##
 ## addTiles will find a tile that matches two sides
@@ -307,176 +184,14 @@ c = 0  # first corner
 ## will NOT be None and the found tile will be rotated/
 ## flipped as required to match, then added to the grid
 ## 
-def addTiles(grid, x, y, t, glo):
+def addTiles(grid, r, c, t, glo):
 	glo.append((t.num, t.cw, t.flipped))
-	for i in range(1,9):
-		for j in range(1,9):
-			gx = 8*(x-1) + i
-			gy = 8*(y-1) + j
-			if t.rows[i][j] == b'#': grid[gx][gy] = 1
+	for i in range(8):
+		for j in range(8):
+			gc = 8*(c) + i
+			gr = 8*(r) + j
+			if t.rows[j+1][i+1] == b'#': grid[gr][gc] = 1
 
-##
-## builds grid from tiles
-##
-def createGridFromTiles(grid, tiles, puzzle, numpixels):
-	for x in range(numpixels):
-		for y in range(numpixels):
-			addTiles(grid, x, y, tiles[x*numpixels + y])
-##
-## add tile to grid\
-##
-def addTileToGrid(grid, tile, x, y):
-	for x in range(numpixels):
-		for y in range(numpixels):
-			addTiles(grid, x, y, tiles[x*numpixels + y])
-
-##
-## start with the corners:
-## -- all corners are type 2
-## -- start with the first corner, rotate it until it is
-## -- False, True, True, False 
-##
-## Build Frame, orient first corner at the upper left
-
-## corners[0].display()
-##
-## Get upper left corner
-##
-if corners[0].xside == [False, False, True, True]:
-	corners[0].rotate(3)
-elif corners[0].xside == [True, False, False, True]:
-	corners[0].rotate(2)
-elif corners[0].xside == [True, True, False, False]:
-	corners[0].rotate(1)
-
-grid = np.zeros((numpixels*8,numpixels*8),dtype=int)
-gridLayOut = []
-
-row = 0
-col = 0
-side = corners[0].side[2]
-addTiles(grid, row, col, corners[0], gridLayOut)
-print("Upper left corner: ", corners[0].num, side)
-corners[0].display()
-corners[0].used = True
-found = False
-##
-## Get left edges, matching side is 2
-for n in range(1,numpixels-1):
-	for e in edges:
-		if e.used: continue
-		found, rot, flip = e.hasSide(side)
-		if found:
-			row += 1
-			e.used = True
-			e.orientToSide(side, 0)
-			addTiles(grid, row, col, e, gridLayOut)
-			print("Next left edge: ", e.num)
-			e.display()
-			side = e.side[2]
-			break
-found = False
-##
-## Bottom row, corner
-for c in corners:
-	if c.used : continue
-	found, rot, flip = c.hasSide(side)
-	if found:
-		row += 1
-		c.used = True
-		c.orientToSide(side, 0)
-		addTiles(grid, row, col, c, gridLayOut)
-		side = c.side[1]
-		print("Lower left corner: ", c.num)
-		c.display()
-		break
-	
-found = False
-##
-## Get bottom edge
-for n in range(1,numpixels-1):
-	for e in edges:
-		if e.used: continue
-		found, rot, flip = e.hasSide(side)
-		if found:
-			col += 1
-			e.used = True
-			e.orientToSide(side, 3)
-			addTiles(grid, row, col, e, gridLayOut)
-			print("Next bottom edge: ", e.num)
-			e.display()
-			side = e.side[1]
-			break
-
-found = False
-##
-## Get lower right edge corner
-for c in corners:
-	if c.used : continue
-	found, rot, flip = c.hasSide(side, True)
-	if found:
-		col += 1
-		c.used = True
-		c.orientToSide(side, 3)
-		addTiles(grid, row, col, c, gridLayOut)
-		side = c.side[0]
-		c.display()
-		print("Lower right corner: ", c.num)
-		break
-		
-found = False
-##
-## Get right edge
-for n in range(1,numpixels-1):
-	for e in edges:
-		if e.used: continue
-		found, rot, flip = e.hasSide(side)
-		if found:
-			row -= 1
-			e.used = True
-			e.orientToSide(side, 2)
-			addTiles(grid, row, col, e, gridLayOut)
-			print("Next right edge: ", e.num)
-			e.display()
-			side = e.side[0]
-			break
-
-found = False
-##
-## Get upper right corder
-for c in corners:
-	if c.used : continue
-	found, rot, flip = c.hasSide(side)
-	if found:
-		row -= 1
-		c.used = True
-		c.orientToSide(side, 2)
-		addTiles(grid, row, col, c, gridLayOut)
-		print("Upper right corner: ", c.num)
-		c.display()
-		side = c.side[3]
-		break
-		
-found = False
-print("*********************************", numpixels)
-##
-## Get right edge
-for n in range(1,numpixels-1):
-	for e in edges:
-		if e.used: continue
-		found, rot, flip = e.hasSide(side)
-		if found:
-			col -= 1
-			e.used = True
-			e.orientToSide(side, 1)
-			addTiles(grid, row, col, e, gridLayOut)
-			print("Next top edge: ", e.num)
-			side = e.side[3]
-			break
-
-#createGridFromTiles(grid, tiles, puzzle, numpixels)
-#addTiles(grid, 0, 0, corner[0])
-#for x in range(1,numpixels-1):	# Do left edges
 
 def countObjects(grid, obj):
 	objx, objy = np.shape(obj)
@@ -491,6 +206,98 @@ def countObjects(grid, obj):
 			#print (x, y, do_sum)
 	return count
 
+
+tiles = []
+f = open("data.txt", "r")
+l = f.readline()
+while not l == "":
+	tiles.append(Tile(f, l))
+	l = f.readline()
+
+def	getSides(tiles, n):
+	for t in tiles:
+		if t.num == n: return t.side
+	return None
+
+prod = 1
+corners = []
+for t in tiles:
+	t.mark(tiles)
+	if (t.type == 2) : 
+		prod *= t.num ## Part 1 corner product
+		corners.append(t)
+		
+
+
+
+print("Part 1: Product is: ", prod)
+
+
+
+##
+## needed for Part 2
+numpixels = math.floor(math.sqrt(len(tiles)) + 0.5)
+puzzle = np.zeros((numpixels, numpixels))
+c = 0  # first corner
+
+grid = np.zeros((numpixels*8,numpixels*8),dtype=int)
+gridLayOut = []
+
+
+##
+## start with the corners:
+## -- all corners are type 2
+## -- start with the first corner, rotate it until it is
+## -- False, True, True, False 
+##
+## Build Frame, orient first corner at the upper left
+
+##
+## Get upper left corner
+##
+if corners[0].xside == [False, False, True, True]:
+	corners[0].rotate(3)
+elif corners[0].xside == [True, False, False, True]:
+	corners[0].rotate(2)
+elif corners[0].xside == [True, True, False, False]:
+	corners[0].rotate(1)
+
+
+row = 0
+col = 0
+side = corners[0].side[1]
+addTiles(grid, row, col, corners[0], gridLayOut)
+corners[0].used = True
+found = False
+snum = 0
+
+def rcnum(r, c, nump):
+	return r * nump + c
+
+#
+# at this point, we have built the edges of the 'puzzle'
+#
+for r in range(numpixels):
+	if r > 0:
+		sides = getSides(tiles, gridLayOut[rcnum(r-1,0,numpixels)][0])
+		side = sides[2]
+		snum = 3
+	for c in range(numpixels):
+		if r == 0 and c == 0: continue
+		if c == 0:
+			snum = 0  # match the top
+		else : 
+			snum = 3
+		for t in tiles:
+			if t.used: continue
+			found, rot, flip = t.hasSide(side)
+			if found:
+				t.used = True
+				t.orientToSide(side, snum)
+				addTiles(grid, r, c, t, gridLayOut)
+				side = t.side[1]
+				break
+
 monster = np.zeros((3,20),dtype=int)
 # 01234567890123456789
 #0                  # 
@@ -500,24 +307,22 @@ monster[0][18] = 1
 for j in (0,5,6,11,12,17,18,19) : monster[1][j] = 1
 for j in (1,4,7,10,13,16) : monster[2][j] = 1
 monsterSize = sum(sum(monster))
-#print("Monster size is ", monsterSize)
 
-#print (monster)
-#print (grid)
-#print(sum(sum(grid)))
+totaldots = sum(sum(grid))
+
 
 for i in range(4):
-	n = countObjects(grid, monster)
+	m = countObjects(grid, monster)
+	if m > 0: numMonsters = m
 	monster = np.rot90(monster)
-	print ("n = ", n)
 
 monster = np.rot90(monster)
 monster = np.fliplr(monster)
 for i in range(4):
-	n = countObjects(grid, monster)
+	m = countObjects(grid, monster)
+	if m > 0: numMonsters = m
 	monster = np.rot90(monster)
-	print ("n = ", n)
 
-print (gridLayOut)
 
-print("Part 2: Number of non-serpant pixels is: ", numpixels)
+print("Part 2: Number of non-serpant pixels is: ", totaldots - monsterSize * numMonsters)
+
