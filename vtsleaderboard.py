435efbc3bd6a7734e5c300507516caa8bc64776a
Dave Buscaglia <david.buscaglia@sbcglobal.net>
2023-11-19 14:24:54 -0800

    Start of automated score board

---
+++ vtsleaderboard.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+'''
+This script will grab the leaderboard from Advent of Code and print it
+'''
+# pylint: disable=wrong-import-order
+# pylint: disable=C0301,C0103,C0209
+
+import os
+import datetime
+import sys
+import json
+import requests
+
+LEADERBOARD_ID = os.environ.get('LEADERBOARD_ID')
+SESSION_ID = os.environ.get('SESSION_ID')
+
+# If the ENV Var hasn't been set, then try to load from local config.
+# Simply create secrets.py with these values defined.
+# See README for more detailed directions on how to fill these variables.
+if not all([LEADERBOARD_ID, SESSION_ID]):
+	from secrets import LEADERBOARD_ID, SESSION_ID
+
+# You should not need to change this URL
+LEADERBOARD_URL = "https://adventofcode.com/{}/leaderboard/private/view/{}".format(
+		datetime.datetime.today().year,
+		LEADERBOARD_ID)
+
+
+def formatLeaderMessage(members):
+	"""
+	Format the message to conform to Slack's API
+	"""
+	message = ""
+
+	# add each member to message
+	medals = [':third_place_medal:', ':second_place_medal:', ':trophy:']
+	for username, score, stars in members:
+		if medals:
+			medal = ' ' + medals.pop()
+		else:
+			medal = ''
+		message += f"{medal}*{username}* {score} Points, {stars} Stars\n"
+
+	message += f"\n<{LEADERBOARD_URL}|View Leaderboard Online>"
+
+	return message
+
+
+def parseMembers(members_json):
+	"""
+	Handle member lists from AoC leaderboard
+	"""
+	# get member name, score and stars
+	members = [(m["name"],
+				m["local_score"],
+				m["stars"]
+				) for m in members_json.values()]
+
+	# sort members by score, descending
+	members.sort(key=lambda s: (-s[1], -s[2]))
+
+	return members
+
+
+def postMessage(message):
+	"""
+	Post the message to to Slack's API in the proper channel
+	"""
+	print (message)
+	
+
+
+def main():
+	"""
+	Main program loop
+	"""
+	# make sure all variables are filled
+	if LEADERBOARD_ID == "" or SESSION_ID == "" :
+		print("Please update script variables before running script.\n\
+				See README for details on how to do this.")
+		sys.exit(1)
+
+	# retrieve leaderboard
+	r = requests.get(
+		"{}.json".format(LEADERBOARD_URL),
+		timeout=60,
+		cookies={"session": SESSION_ID}
+	)
+	if r.status_code != requests.codes.ok:  # pylint: disable=no-member
+		print("Error retrieving leaderboard")
+		sys.exit(1)
+
+	# get members from json
+	members = parseMembers(r.json()["members"])
+
+	# generate message to send to slack
+	message = formatLeaderMessage(members)
+
+	# send message to slack
+	postMessage(message)
+
+
+if __name__ == "__main__":
+	main()
