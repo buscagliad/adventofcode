54d2c3ca96efca2d3cb8886e6dd2d20f1c879b11
Dave Buscaglia <david.buscaglia@sbcglobal.net>
2023-11-24 13:48:13 -0800

    still working on day19

--- 2015/day19/chemistry.py
+++ 2015/day19/chemistry.py
@@ -1,3 +1,4 @@
+import copy
 
 chems={}
 basechem=""
@@ -16,17 +17,20 @@ def chemicals(chem, f, to):
 
 def rem(chem, f, to):
 	r = 0
+	n = 0
+	i = 0
 	d = len(f)
-	retset={}
-	while True:
-		i = chem.find(f, r)
+	newch = copy.deepcopy(chem)
+	while i < len(chem) - 1:
+		i = newch.find(f)
 		if (i < 0) : break
-		newch = ''.join([chem[:i], to, chem[i+d:]])
-		if len(retset) == 0: retset={newch}
-		else: retset.add(newch)
-		r = i + 1
+		print("Found ", f, " at ", i, " replaced with ", t)
+		n += 1
+		print("old chem: ", newch)
+		newch = ''.join([newch[:i], to, newch[i+d:]])
+		print("new chem: ", newch)
 	#print("retset is ", retset)
-	return retset
+	return newch, n
 
 fromto = True
 
@@ -64,33 +68,127 @@ chems.clear()
 chems={basechem}
 numsteps = 0
 done = False
+
+def getatoms(s):
+	atoms=[]
+	for t, f in s:
+		if "Rn" in f:
+			atoms.append([t,f])
+	print(atoms)
+	return atoms
+
+atoms = getatoms(steps)
+print(basechem)
+
+def ARsplit(s):
+	spaces = ""
+	rndepth = -1
+	rl = -1
+	rr = -1
+	returnl = -1
+	returnr = -1
+	rdepth = 0
+	for i in range(len(s)-1):
+		if s[i:i+2] == "Rn":
+			if rr < 0: rdepth += 1
+			print("  "*rdepth, "Rn found at ", i)
+			rl = i - 2
+			rr = -1
+		if s[i:i+2] == "Ar":
+			if rndepth < rdepth:
+				rndepth = rdepth
+				returnl = rl 
+				returnr = i + 2
+			if rr > 0: 			
+				rdepth -= 1
+			print("  "*rdepth, "Ar found at ", i)
+			rr = i + 2
+	print("ARsplit returning ", returnl, returnr)
+	return returnl, returnr
+
+if "Bi" in basechem:
+	print("Bi found 1")
+	exit(1)
+	
+done = False
+while not basechem == "e" and not done:
+	done = True
+	for t,f in atoms:
+		basechem, k = rem(basechem, f, t)
+		numsteps += k
+		if k > 0: done = False
+		print(f, "  appears: ", k, " times.", " total: ", numsteps, flush=True)
+		if "Bi" in basechem:
+			print("Bi found 2", t, f, numsteps)
+			exit(1)
+#
+# grab xxRn...Ar molecules as see what they have inside them
+#
+def grabRn(s, nAr = 1):
+	r = s.find("Ar") + 2
+	for j in range(nAr):
+		l = s.find("Rn")
+	ll = l
+	while ll < r:
+		ll = s.find("Rn", l + 1)
+		if ll < r: l = ll
+	if s[l-1].islower(): l -= 2
+	else: l -= 1
+	print(l, r)
+	print(s[:l], s[l:r], s[r:], sep="")
+	return s[l:r], s[:l], s[r:]
+
+def reduct(s):
+	global steps
+	num = 0
+	done = False
+	while not done:
+		done = True
+		for t, f in steps:
+			s, k = rem(s, f, t)
+			num += k
+			if k > 0: done = False
+	return s, num
+	 
+print (basechem)
+
+rn, l, r = grabRn(basechem)
+n = 1
+nAr = 1
+while len(rn) and n > 0:
+	s, n = reduct(rn)
+	print("grabRn: ", rn)
+	print("Reduced to ", s, n)
+	basechem = ''.join([l, s, r])
+	print("NAMES: ", "|", l, "|", s, "|", r, "|", sep="")
+	print (basechem)
+	if "Bi" in basechem:
+		print("Bi found 3", s, n, numsteps)
+		exit(1)
+	numsteps += n
+	if n == 0: 
+		nAr += 1
+		n = 1 # keep look going
+		print("Increased nAr to: ", nAr)
+	rn, l, r = grabRn(basechem, nAr)
+exit(1)
+
+def chsplit(s):
+	l = s.find("Rn") - 1
+	r = s.find("Ar") + 2
+	a = s[l]
+	if a.islower(): l -= 1
+	m = s[l:r]
+	return ''.join([s[:l], s[r:]]), m
+	
+done = False
+ARsplit(basechem)
+
+exit(1)
+
 while not done:
-	numsteps += 1
-	newset = {}
-	cc = 0
-	for c in chems:
-		for t,f in steps:
-			cc += c.count(f)
-	print("Number of chems: ", len(chems), " number of t.f steps: ", len(steps), " new number of chems expected:", cc)
-	for c in chems:
-		for t,f in steps:
-			if f in c:
-				s = rem(c, f, t)
-				#print("REM: ", s)
-				if len(newset) == 0:
-					newset = s
-				else:
-					newset = newset.union(s)
-		#print("...Step: ", numsteps, " adding: ", len(s), " total: ", len(newset), flush=True)
-	#print("NEWSET::", newset)
-	chems.clear()
-	chems=newset
-	#print("CHEMS:", chems)
-	print("Step: ", numsteps, " chems: ", len(chems), flush=True)
-	if "e" in chems: done = True
-	if (len(chems) == 0) : done = True
+	basechem, molecule = chsplit(basechem)
+	print(molecule, flush=True)
 
 print ("Part2: creating \"e\" took", numsteps, "steps")
-#chems.clear{}
-#while basechem not in chems:
-	
+
