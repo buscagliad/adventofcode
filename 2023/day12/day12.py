288ce81b6cc5f03636be1e4b351e1c76208aa30e
Dave Buscaglia <david.buscaglia@sbcglobal.net>
2023-12-12 08:06:51 -0800

    day 12 still working

--- 2023/day12/day12.py
+++ 2023/day12/day12.py
@@ -48,10 +48,11 @@ def play(s, x):
 
 
 def doline(line, w):
-	count = 0
-	for i in range(len(line)):
-		if sline(line[i:], w, 0) : count += 1
-	return count
+	return sline(line, w)
+#	count = 0
+#	for i in range(len(line)):
+#		if sline(line[i:], w, 0) : count += 1
+#	return count
 
 
 def process(line):
@@ -77,9 +78,42 @@ def process2(line):
 	return doline(fields, m)
 
 
-def sline(line, w, wi, rec=False):
-	#if rec: print("   ", end="")
-	#print("sline: ", line, " w[", wi, "] = ", w[wi])
+def sline(pline, w, li=0, wi=0, sindex=0, rec=False):
+	line=pline[li:]
+	if rec: print("   ", end="")
+	print("sline: ", line, " w[", wi, "] = ", w[wi])
+	if wi + 1 == len(w) and line.count('#') == 0:
+		sindex += 1
+		return 1 + sline(line, w, sindex, wi+1, sindex, True)
+	if len(line) == 0: return 0
+	c = 0
+	i = 0
+	while i < len(line) and line[i] == '.': i += 1
+	questionmark = line[i] == '?'
+	qindex = i
+	print("Q found", qindex)
+	if i >= len(line): return 0
+	for i in range(len(line)):
+		if line[i] == '#' or line[i] == '?' : c += 1
+		print("i: ", i, " c: ", c, " wi: ", wi)
+		if c == w[wi] :
+			if wi == len(w) - 1: 
+				print("TRUE")
+				return 1 + sline(line, w, sindex+1, wi+1, sindex+1, True)
+				
+			if i < len(line) - 1 and not line[i+1] == '#':
+				if questionmark:
+					return sline(line, w,      i+2, wi+1, sindex, True) + \
+						   sline(line, w, qindex+1, wi+1, sindex, True)
+				return     sline(line, w,      i+2, wi+1, sindex, True)
+
+			return sline(line, w, sindex+1, wi+1, sindex+1, True)
+			
+	return sline(line, w, sindex+1, wi+1, sindex+1, True)
+
+def slinex(line, w, wi, rec=False):
+	if rec: print("   ", end="")
+	print("sline: ", line, " w[", wi, "] = ", w[wi])
 	if wi + 1 == len(w) and line.count('#') == 0:
 		return True
 	if len(line) == 0: return False
@@ -97,14 +131,14 @@ def sline(line, w, wi, rec=False):
 				return sline(line[i+2:], w, wi+1, True)
 			return False
 
-print(process("?###???????? 3,2,1"))
-print(process2("?###???????? 3,2,1"))
+print("Part 1 process: ", process("?###???????? 3,2,1"))
+print("Part 2 process: ", process2("?###???????? 3,2,1"))
 exit(1)
 count = 0
 for line in open("test.txt"):
 	print(line)
 	print("Part 1 process: ", process(line))
-	print("Part 2 process: ", process2(line))
+	print("Part 2 process: ", process2(line) )
 	#count += process(line)
 	
 print ("Part 1: Number of possible valid arrangements: ", count)
