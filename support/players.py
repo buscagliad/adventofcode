7a8a7f7d52635660251fbcaadd3b80ee259ccc69
Dave Buscaglia <david.buscaglia@vts-i.com>
2024-11-27 10:57:55 -0800

    VTS leaderboard

--- support/players.py
+++ support/players.py
@@ -1,5 +1,6 @@
 import time
 
+'''
 Roster = {}
 Roster[3187848] = ["Phil Calora", "philip.calora@vts-i.com"]
 Roster[1540194] = ["Matt Binning", "matthew.binning@vts-i.com"]
@@ -31,20 +32,86 @@ Roster[2246495] = ["Sean McCarthy", "sean.mccarthy@vts-i.com"]
 Roster[2258724] = ["Mattie Sanz", "mattie.sanz@vts-i.com"]
 Roster[3690597] = ["Matthew Luckenbihl", "matthew.luckenbihl@vts-i.com"]
 Roster[3853633] = ["Sean Severson", "sean.severson@vts-i.com"]
+'''
 
-def getPlayerName(idx):
-	global Roster
-	if Roster[idx] is None:
-		print("ERROR - so such player index: ", idx)
-		return "ERROR"
-	return Roster[idx][0]
 
-def getPlayerEmail(idx):
-	global Roster
-	if Roster[idx] is None:
-		print("ERROR - so such player index: ", idx)
-		return "ERROR"
-	return Roster[idx][1]
+class Player:
+    def __init__(self, idx, numstars, laststar):
+        global roster
+        self.idx = idx
+        self.name = roster.getPlayerName(idx)
+        self.laststar = laststar
+        self.numstars = numstars
+        self.email = roster.getPlayerEmail(idx)
+        self.problems = [[0,0]]*26
+        self.lastStarDate = 0
+        self.lastPuzzle = ""
+
+    def addDay(self, day, p1, p2=0):
+        self.problems[day] = [int(p1), int(p2)]
+        # print(self.name, " adding day ", day, " times: ", int(p1), int(p2))
+        if p1 > self.lastStarDate:
+            self.lastStarDate = p1
+            self.lastPuzzle = "Day " + str(day) + " Part 1"
+        if p2 > self.lastStarDate:
+            self.lastStarDate = p2
+            self.lastPuzzle = "Day " + str(day) + " Part 2"
+    def out(self):
+        print("***  ", self.name)
+        print()
+        tm = time.localtime(self.laststar)
+        stm = time.strftime("%d %b %Y %H:%M:%S", tm)
+        print(" Number of stars: ", self.numstars, end="")
+        if (self.numstars > 0) : print ("      Last Star: ", stm);
+        else:  print()
+        for d in range(len(self.problems)):
+            t1, t2 = self.problems[d]
+            if t1 == 0 and t2 == 0: continue
+            sp = ""
+            if d < 10: sp = " "
+            
+            print("   Day ", sp, d, end = "", sep = "")
+            print("   Part 1: ", pltime(t1), end = "", sep = "")
+            if t2 > 0:
+                print("   Part 2: ", pltime(t2), end = "", sep = "")
+            print()
+        print()
+
+    def simpleout(self, n):
+        if (n == 0):
+            print("    Name                        # Stars  Last Star Achieved")
+            return
+        tm = time.localtime(self.laststar)
+        stm = time.strftime("%d %b %Y %H:%M:%S", tm)
+        if (self.numstars == 0) : stm = ""
+        print(f'{n:2d}  {self.name:30}  {self.numstars:3d}  {stm}')
+
+
+class Roster:
+    def __init__(self, fname):
+        self.Roster = {}
+        for l in open(fname):
+            w = l.strip().split(',')
+            self.Roster[int(w[0])] = [w[1],w[2]]
+
+    def getPlayerName(self, idx):
+        if idx in self.Roster.keys():
+            return self.Roster[idx][0]
+        else:
+            print("ERROR - XXX no such player index: ", idx)
+            return "ERROR"
+
+    def getPlayerEmail(self, idx):
+        if idx in self.Roster.keys():
+            return self.Roster[idx][1]
+        else:
+            print("ERROR - YYY no such player index: ", idx)
+            return "ERROR"
+            
+    def out(self):
+        print(self.Roster.keys())
+        for k, v in self.Roster.items():
+            print(k, v)
 
 #
 # returns True if p1 is better than p2
@@ -52,67 +119,19 @@ def getPlayerEmail(idx):
 # last star was earlier
 #
 def plcomp(p1, p2):
-	if p1.numstars == p2.numstars:
-		if p1.laststar < p2.laststar:
-			return -1
-		else:
-			return 1
-	if p1.numstars > p2.numstars:
-		return -1
-	else:
-		return 1
+    if p1.numstars == p2.numstars:
+        if p1.laststar < p2.laststar:
+            return -1
+        else:
+            return 1
+    if p1.numstars > p2.numstars:
+        return -1
+    else:
+        return 1
 
 def pltime(t):
-	tm = time.localtime(t)
-	stm = time.strftime("%d %b %Y %H:%M:%S", tm)
-	return stm
+    tm = time.localtime(t)
+    stm = time.strftime("%d %b %Y %H:%M:%S", tm)
+    return stm
 
-class Player:
-	def __init__(self, idx, numstars, laststar):
-		self.idx = idx
-		self.name = getPlayerName(idx)
-		self.laststar = laststar
-		self.numstars = numstars
-		self.email = getPlayerEmail(idx)
-		self.problems = [[0,0]]*26
-		self.lastStarDate = 0
-		self.lastPuzzle = ""
-
-	def addDay(self, day, p1, p2=0):
-		self.problems[day] = [int(p1), int(p2)]
-		# print(self.name, " adding day ", day, " times: ", int(p1), int(p2))
-		if p1 > self.lastStarDate:
-			self.lastStarDate = p1
-			self.lastPuzzle = "Day " + str(day) + " Part 1"
-		if p2 > self.lastStarDate:
-			self.lastStarDate = p2
-			self.lastPuzzle = "Day " + str(day) + " Part 2"
-	def out(self):
-		print("***  ", self.name)
-		print()
-		tm = time.localtime(self.laststar)
-		stm = time.strftime("%d %b %Y %H:%M:%S", tm)
-		print(" Number of stars: ", self.numstars, end="")
-		if (self.numstars > 0) : print ("      Last Star: ", stm);
-		else:  print()
-		for d in range(len(self.problems)):
-			t1, t2 = self.problems[d]
-			if t1 == 0 and t2 == 0: continue
-			sp = ""
-			if d < 10: sp = " "
-			
-			print("   Day ", sp, d, end = "", sep = "")
-			print("   Part 1: ", pltime(t1), end = "", sep = "")
-			if t2 > 0:
-				print("   Part 2: ", pltime(t2), end = "", sep = "")
-			print()
-		print()
-
-	def simpleout(self, n):
-		if (n == 0):
-			print("    Name                        # Stars  Last Star Achieved")
-			return
-		tm = time.localtime(self.laststar)
-		stm = time.strftime("%d %b %Y %H:%M:%S", tm)
-		if (self.numstars == 0) : stm = ""
-		print(f'{n:2d}  {self.name:30}  {self.numstars:3d}  {stm}')
+roster = Roster("roster.csv")
--- support/vtsleaderboard.py
+++ support/vtsleaderboard.py
@@ -21,155 +21,157 @@ SESSION_ID = os.environ.get('SESSION_ID')
 # Simply create secrets.py with these values defined.
 # See README for more detailed directions on how to fill these variables.
 if not all([LEADERBOARD_ID, SESSION_ID]):
-	from secrets import LEADERBOARD_ID, SESSION_ID, AOC_YEAR
+    from secrets import LEADERBOARD_ID, SESSION_ID, AOC_YEAR
 
 # You should not need to change this URL
 LEADERBOARD_URL = "https://adventofcode.com/{}/leaderboard/private/view/{}".format(
-		AOC_YEAR,
-		LEADERBOARD_ID)
+        AOC_YEAR,
+        LEADERBOARD_ID)
 
 
 
 def printMembers(members):
-	"""
-	Format the message to conform to Slack's API
-	"""
-	message = ""
+    """
+    Format the message to conform to Slack's API
+    """
+    message = ""
 
-	# add each member to message
-	medals = [':third_place_medal:', ':second_place_medal:', ':trophy:']
-	for username, score, stars in members:
-		if medals:
-			medal = ' ' + medals.pop()
-		else:
-			medal = ''
-		message += f"{medal}*{username}* {score} Points, {stars} Stars\n"
+    # add each member to message
+    medals = [':third_place_medal:', ':second_place_medal:', ':trophy:']
+    for username, score, stars in members:
+        if medals:
+            medal = ' ' + medals.pop()
+        else:
+            medal = ''
+        message += f"{medal}*{username}* {score} Points, {stars} Stars\n"
 
 
-	return message
+    return message
 
 
 def parseMembers(members_json):
-	"""
-	Handle member lists from AoC leaderboard
-	"""
-	# get member name, score and stars
-	members = [(m["name"],
-				m["local_score"],
-				m["stars"]
-				) for m in members_json.values()]
+    """
+    Handle member lists from AoC leaderboard
+    """
+    # get member name, score and stars
+    members = [(m["name"],
+                m["local_score"],
+                m["stars"]
+                ) for m in members_json.values()]
 
-	# sort members by score, descending
-	members.sort(key=lambda s: (-s[1], -s[2]))
+    # sort members by score, descending
+    members.sort(key=lambda s: (-s[1], -s[2]))
 
-	return members
+    return members
 
 def playerList(members_json):
-	members = [(m["name"],
-				m["stars"],
-				m["last_star_ts"],
-				m["id"],
-				m["completion_day_level"]
-				) for m in members_json.values()]
-	plist = []
-	for name, stars, laststar, playerID, dayResults in members:
-		#print(playerID, name, pl.getPlayerName(playerID))
-		p = pl.Player(playerID, stars, laststar)
-		#print("**************** ", name, " ************************")
-		#print()
-		tm = time.localtime(laststar)
-		stm = time.strftime("%d %b %Y %H:%M:%S", tm)
-		#print(" Number of stars: ", stars, " Last Star: ", stm);
-		#print(dayResults)
-		#for day in dayResults:
-			#print("3", " a: ", day, "  ", dayResults[day])
-			#for k in dayResults[day]:
-				#tm = time.localtime(dayResults[day][k]["get_star_ts"])
-				#stm = time.strftime("%d %b %Y %H:%M:%S", tm)
-				#print("k ", k, dayResults[day][k]["get_star_ts"], stm)
+    members = [(m["name"],
+                m["stars"],
+                m["last_star_ts"],
+                m["id"],
+                m["completion_day_level"]
+                ) for m in members_json.values()]
+    plist = []
+    for name, stars, laststar, playerID, dayResults in members:
+        #print(playerID, name, pl.getPlayerName(playerID))
+        p = pl.Player(playerID, stars, laststar)
+        #print("**************** ", name, " ************************")
+        #print()
+        tm = time.localtime(laststar)
+        stm = time.strftime("%d %b %Y %H:%M:%S", tm)
+        #print(" Number of stars: ", stars, " Last Star: ", stm);
+        #print(dayResults)
+        #for day in dayResults:
+            #print("3", " a: ", day, "  ", dayResults[day])
+            #for k in dayResults[day]:
+                #tm = time.localtime(dayResults[day][k]["get_star_ts"])
+                #stm = time.strftime("%d %b %Y %H:%M:%S", tm)
+                #print("k ", k, dayResults[day][k]["get_star_ts"], stm)
 
-		for day in dayResults:
-			t1 = 0
-			t2 = 0
-			for part in dayResults[day]:
-				if part == '1': t1 = dayResults[day][part]["get_star_ts"]
-				if part == '2': t2 = dayResults[day][part]["get_star_ts"]
-			#print(day, t1, t2, part)
-			p.addDay(int(day), t1, t2)
-		plist.append(p)
-	plist.sort(key=functools.cmp_to_key(pl.plcomp))
-	return plist
+        for day in dayResults:
+            t1 = 0
+            t2 = 0
+            for part in dayResults[day]:
+                if part == '1': t1 = dayResults[day][part]["get_star_ts"]
+                if part == '2': t2 = dayResults[day][part]["get_star_ts"]
+            #print(day, t1, t2, part)
+            p.addDay(int(day), t1, t2)
+        plist.append(p)
+    plist.sort(key=functools.cmp_to_key(pl.plcomp))
+    return plist
 
 def outCSV(members_json):
-	"""
-	Handle member lists from AoC leaderboard
-	"""
-	# get member name, score and stars
-	members = [(m["name"],
-				m["local_score"],
-				m["stars"],
-				m["last_star_ts"],
-				m["completion_day_level"]
-				) for m in members_json.values()]
-	for q in members:
-		print()
-		print("**************** ", q[0], " ************************")
-		print()
-		tm = time.localtime(q[3])
-		stm = time.strftime("%d %b %Y %H:%M:%S", tm)
+    """
+    Handle member lists from AoC leaderboard
+    """
+    # get member name, score and stars
+    members = [(m["name"],
+                m["local_score"],
+                m["stars"],
+                m["last_star_ts"],
+                m["completion_day_level"]
+                ) for m in members_json.values()]
+    for q in members:
+        print()
+        print("**************** ", q[0], " ************************")
+        print()
+        tm = time.localtime(q[3])
+        stm = time.strftime("%d %b %Y %H:%M:%S", tm)
 
-		print("Score: ", q[1], " Number of stars: ", q[2], " Last Star: ", stm);
-		for j in q[4]:
-			print("3", " j: ", j, "  ", q[4][j])
-			for k in q[4][j]:
-				tm = time.localtime(q[4][j][k]["get_star_ts"])
-				stm = time.strftime("%d %b %Y %H:%M:%S", tm)
-				print("k ", q[4][j][k]["get_star_ts"], stm)
-		print()
-		print()
-			#print(j)
-		#if not q[3]["3"] is None:
-		#	print(q[3]["3"])
+        print("Score: ", q[1], " Number of stars: ", q[2], " Last Star: ", stm);
+        for j in q[4]:
+            print("3", " j: ", j, "  ", q[4][j])
+            for k in q[4][j]:
+                tm = time.localtime(q[4][j][k]["get_star_ts"])
+                stm = time.strftime("%d %b %Y %H:%M:%S", tm)
+                print("k ", q[4][j][k]["get_star_ts"], stm)
+        print()
+        print()
+            #print(j)
+        #if not q[3]["3"] is None:
+        #   print(q[3]["3"])
 
-		#print(pretty_json)
-		#for g in m[3]:
-		#	print("Game ", g)
-		#	for d in g:
-		#		print("Day ", d)
-	
+        #print(pretty_json)
+        #for g in m[3]:
+        #   print("Game ", g)
+        #   for d in g:
+        #       print("Day ", d)
+    
 
 def main():
-	"""
-	Main program loop
-	"""
-	# make sure all variables are filled
-	if LEADERBOARD_ID == "" or SESSION_ID == "" :
-		print("Please update script variables before running script.\n\
-				See README for details on how to do this.")
-		sys.exit(1)
+    """
+    Main program loop
+    """
 
-	# retrieve leaderboard
-	r = requests.get(
-		"{}.json".format(LEADERBOARD_URL),
-		timeout=60,
-		cookies={"session": SESSION_ID}
-	)
-	if r.status_code != requests.codes.ok:  # pylint: disable=no-member
-		print("Error retrieving leaderboard", LEADERBOARD_URL)
-		sys.exit(1)
+    
+    # make sure all variables are filled
+    if LEADERBOARD_ID == "" or SESSION_ID == "" :
+        print("Please update script variables before running script.\n\
+                See README for details on how to do this.")
+        sys.exit(1)
 
-	# get members from json
-	members = parseMembers(r.json()["members"])
-	
-	
-	# output csv from JOSN
-	# outCSV(r.json()["members"])
-	pList = playerList(r.json()["members"])
-	n = 0
-	pList[0].simpleout(n)
-	for p in pList:
-		n += 1
-		p.simpleout(n)
+    # retrieve leaderboard
+    r = requests.get(
+        "{}.json".format(LEADERBOARD_URL),
+        timeout=60,
+        cookies={"session": SESSION_ID}
+    )
+    if r.status_code != requests.codes.ok:  # pylint: disable=no-member
+        print("Error retrieving leaderboard", LEADERBOARD_URL)
+        sys.exit(1)
+
+    # get members from json
+    members = parseMembers(r.json()["members"])
+    
+    
+    # output csv from JOSN
+    # outCSV(r.json()["members"])
+    pList = playerList(r.json()["members"])
+    n = 0
+    pList[0].simpleout(n)
+    for p in pList:
+        n += 1
+        p.simpleout(n)
 
 if __name__ == "__main__":
-	main()
+    main()
