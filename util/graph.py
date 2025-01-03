83281441b098fa4bb648d3d6caefb7bd5dfafc77
Dave Buscaglia <david.buscaglia@vts-i.com>
2025-01-03 15:08:15 -0800

    graph.py is a work in progress

--- util/graph.py
+++ util/graph.py
@@ -14,7 +14,7 @@ class Graph:
         self.directed = directed
         self.debug = False
     def __str__(self):
-        self.printGraph()
+        self.print()
     def addedge(self, vertA, vertB, dist):
         if not vertA in self.vertices:
             self.vertices.append(vertA)
@@ -85,7 +85,117 @@ class Graph:
                     if (self.debug) : print("***", current, " -> ", adjNode, " == ", newDist)
                     heap.heappush(q, adjNode)
         return distance[stop]  
+
+    # A Python Program to detect
+    # cycle in an undirected graph
+
+    def is_cyclic_util(self, v, visited, parent):
+      
+        # Mark the current node as visited
+        visited[v] = True
+
+        # Recur for all the vertices
+        # adjacent to this vertex
+        for i in self.adj[v]:
+          
+            # If an adjacent vertex is not visited,
+            # then recur for that adjacent
+            if not visited[i]:
+                if is_cyclic_util(i, adj, visited, v):
+                    return True
+                  
+            # If an adjacent vertex is visited and
+            # is not parent of current vertex,
+            # then there exists a cycle in the graph.
+            elif i != parent:
+                return True
+
+        return False
+
+    def is_cyclic(self):
+        V = len(self.vertices)
+        # Mark all the vertices as not visited
+        visited = [False] * V
+
+        # Call the recursive helper function
+        # to detect cycle in different DFS trees
+        for u in range(V):
+          
+            # Don't recur for u if it is already visited
+            if not visited[u]:
+                if self.is_cyclic_util(u, visited, -1):
+                    return True
+
+        return False
+
         
+# A Python Program to detect
+# cycle in an undirected graph
+
+def is_cyclic_util(v, adj, visited, parent):
+  
+    # Mark the current node as visited
+    visited[v] = True
+
+    # Recur for all the vertices
+    # adjacent to this vertex
+    for i in adj[v]:
+      
+        # If an adjacent vertex is not visited,
+        # then recur for that adjacent
+        if not visited[i]:
+            if is_cyclic_util(i, adj, visited, v):
+                return True
+              
+        # If an adjacent vertex is visited and
+        # is not parent of current vertex,
+        # then there exists a cycle in the graph.
+        elif i != parent:
+            return True
+
+    return False
+
+def is_cyclic(V, adj):
+  
+    # Mark all the vertices as not visited
+    visited = [False] * V
+
+    # Call the recursive helper function
+    # to detect cycle in different DFS trees
+    for u in range(V):
+      
+        # Don't recur for u if it is already visited
+        if not visited[u]:
+            if is_cyclic_util(u, adj, visited, -1):
+                return True
+
+    return False
+
+# Driver program to test above functions
+def testCycle():
+    V = 3
+    adj = [[] for _ in range(V)]
+
+    adj[1].append(0)
+    adj[0].append(1)
+    adj[0].append(2)
+    adj[2].append(0)
+    adj[1].append(2)
+    adj[2].append(1)
+
+    print("Contains cycle" if is_cyclic(V, adj) else "No Cycle")
+
+    V = 3
+    adj2 = [[] for _ in range(V)]
+
+    adj2[0].append(1)
+    adj2[1].append(0)
+    adj2[1].append(2)
+    adj2[2].append(1)
+
+    print("Contains Cycle" if is_cyclic(V, adj2) else "No Cycle")
+
+
 def test():
     g = Graph()
     g.addedge((0, 1), (5, 3), 15)
@@ -105,4 +215,5 @@ def test():
     print("Get min distance: ", g.getmindistance((0, 1), (22, 21)))
     print("Get max distance: ", g.getmaxdistance((0, 1), (22, 21)))
 
-test()
+
+testCycle()
