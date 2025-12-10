6f0587f97469b5b133978578abc14aaaca4775bf
Dave Buscaglia <david.buscaglia@vts-i.com>
2025-12-09 18:06:57 -0800

    day 08 still struggling

--- 2025/day08/playground.py
+++ 2025/day08/playground.py
@@ -42,6 +42,14 @@ def init(fname):
     #pvals()
 
 
+def pcircs(nc, prnt = False):
+    global nodes, nodecount  
+    vv = [0] * circid
+    for i in range(nodecount):
+        vv[nodes[i,3]] += 1
+    if prnt: print(vv)
+    return vv
+
 def dist(a, b):
     return (a[0]-b[0])**2 + (a[1]-b[1])**2 + (a[2]-b[2])**2
 
@@ -56,37 +64,34 @@ def getjunction():
     mindist = 1000000000000000000
     n1 = -1
     n2 = -1
-    common_nodes = 0
     for i in range(nodecount):
-        for j in range(i):
-            if nodes[i,3] > 0 and nodes[j,3] == nodes[i,3] :
-                #common_nodes += 1
-                #print("COM NODES", common_nodes)
-                continue
+        if nodes[i][3] > 0: continue
+        for j in range(nodecount):
+            if j == i: continue
             d = dists[i,j]
-            if d < mindist:
+            if d > 0 and d < mindist:
                 n1 = i
                 n2 = j
-                mindist = d            
-    return common_nodes, n1, n2
+                mindist = d
+    if n1 >= 0 and n2 >= 0:
+        dists[n1,n2] = 0
+        dists[n2,n1] = 0
+    return n1, n2
 
-init('data.txt')
+init('test.txt')
 n1 = 0
 n2 = 0
 compdists()
 circid = 1
-cnter = 0
 allconnected = False
-while not allconnected:
-    cns, n1, n2 = getjunction()
-    cnter += 1 + cns
-    bypass = True
+#while not allconnected:
+for q in range(10):
+    n1, n2 = getjunction()
     if (n1 < 0): break
     ## merge nodes use node n1 and the merger
     if nodes[n1,3] > 0 and nodes[n2,3] > 0:
         if nodes[n1,3] == nodes[n2,3]:
-            bypass = False
-            print("Bypassing")
+            print("Ignoring node ", nodes[n2], " to node ", nodes[n1])
             continue
 
         print("Merging node ", nodes[n2,3], n2, " to node ", nodes[n1,3], n1)
@@ -95,7 +100,7 @@ while not allconnected:
         for i in range(nodecount):
             if nodes[i,3] == fromnode:
                 nodes[i,3] = tonode
-                print("XXXX", i, n2, nodes[i,3], nodes[n1,3])
+                #print("XXXX", i, n2, nodes[i,3], nodes[n1,3])
     elif nodes[n1,3] > 0:
         nodes[n2,3] = nodes[n1,3]
         print("Junction node: ", n2, nodes[n2], " added to junction ", n1, nodes[n1])
@@ -111,12 +116,11 @@ while not allconnected:
         nodes[n2,3] = circid
         print("Junction created: ", circid, " NODES are: ", n1, nodes[n1], " and ", n2, nodes[n2])
         circid += 1
+    pcircs(circid, True)
     allconnected = countcon()
+
     
-vv = [0] * circid
-for i in range(nodecount):
-    vv[nodes[i,3]] += 1
-print(vv)
+vv = pcircs(circid)
 vv[0] = 0
 vv.sort(reverse=True)
 print(vv)
